6c2c755f9e0c827584dd5f6b15e2fe0e3b62699d
Khalil Mansour <127430852+KhalilMansour01@users.noreply.github.com>
2025-04-04 08:34:02 +0300

    Initial commit

---
+++ src/test/java/com/example/club_connect/ClubConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.club_connect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
